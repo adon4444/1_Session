3c721655fa2cb84f561bcab760fe7e39adeef37c
Peggy Lindner <peggylind@gmail.com>
2019-08-29 12:06:44 -0500

    Initial version of code. We will only put .java files (source code not byte code .class files).

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println(1+5);
+        System.out.print("What happens now?");
+        System.out.print("... and now?");
+    }
+}
